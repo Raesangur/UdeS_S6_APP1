ef58a071a68b567935f8900a0c305249610f8d69
raesangur <pascalemmanuel.lachance@gmail.com>
2023-05-02 16:07:07 -0400

    partie 1; multithreading fonctionnel dans asset_conv avec benchmark

---
+++ code_base/src/benchmark.h
@@ -0,0 +1,42 @@
+#pragma once
+/*****************************************************************************/
+/* Includes ---------------------------------------------------------------- */
+#include <chrono>
+#include <iostream>
+
+
+/*****************************************************************************/
+/* Defines ----------------------------------------------------------------- */
+#define BENCHMARK_CODE true
+
+
+/*****************************************************************************/
+/* Classes ----------------------------------------------------------------- */
+class Benchmark
+{
+private:
+    using clock     = std::chrono::high_resolution_clock;
+    using timepoint = std::chrono::time_point<clock>;
+    using duration  = std::chrono::duration<uint64_t, std::milli>;
+
+    timepoint   m_startTime;
+    const char* m_msg;
+
+public:
+    Benchmark(const char* msg = "") : m_msg{msg}, m_startTime{clock::now()}
+    {
+    }
+    ~Benchmark()
+    {
+        using namespace std::chrono;
+
+        timepoint endTime = clock::now();
+        duration  dur     = duration_cast<duration>(endTime - m_startTime);
+
+        std::cout << m_msg << " Took: " << dur.count() << "ms" << std::endl;
+    }
+};
+
+
+/*****************************************************************************/
+/* END OF FILE ------------------------------------------------------------- */
